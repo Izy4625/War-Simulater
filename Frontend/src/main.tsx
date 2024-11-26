2e6613005911cb19ca4537f3e0284c40bb592646
yisroel gold <yisoelgold3@gmail.com>
2024-11-26 15:11:30 +0200

    did npm i

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 
 
-// setupAxios();
+
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
